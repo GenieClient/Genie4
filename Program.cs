393b758566489e408e139a37f775ea88f9aec495
dennis <dennis@stafford.us>
2022-01-04 17:37:20 -0700

    Setup for dependency injection and redirect launch of FormMain from program.cs. This will make some other classes obsolete.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows.Forms;
+
+namespace GenieClient
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    ConfigureServices(context.Configuration, services);
+                })
+                .Build();
+
+            var services = host.Services;
+            var formMain = services.GetRequiredService<FormMain>();
+
+            Application.Run(formMain);
+        }
+
+        private static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddSingleton<FormMain>();
+
+            //Register services here. 
+            //Example... Service.AddSingleton(Interface, Service);
+        }
+    }
+}
