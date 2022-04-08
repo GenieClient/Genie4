3d44321efc4bbcbf94b4e2182a334a8435d5b39c
Djordje <cyphertinker@gmail.com>
2022-04-07 22:35:56 -0400

    Changed method for inserting prompt to populate prompt with data

--- Forms/FormMain.cs
+++ Forms/FormMain.cs
@@ -4522,7 +4522,6 @@ namespace GenieClient
         private void AddText(string sText, Color oColor, Color oBgColor, FormSkin oTargetWindow, bool bNoCache = true, bool bMono = false, bool bPrompt = false, bool bInput = false)
         {
             if (sText == "\r\n" && m_oGlobals.Config.Condensed) return;
-            bPrompt = false;
 
             if (IsDisposed)
             {
@@ -4538,17 +4537,13 @@ namespace GenieClient
             {
                 if (bPrompt == true)
                 {
-                    if (m_oGame.LastRowWasPrompt)
-                    {
-                        return;
-                    }
-
                     m_oGame.LastRowWasPrompt = true;
                 }
                 else if (sText.Trim().Length > 0)
                 {
                     if (m_oGame.LastRowWasPrompt == true)
                     {
+                        m_oGame.LastRowWasPrompt = false;
                         if (!bInput)
                         {
                             if (sText.StartsWith(System.Environment.NewLine) == false && m_oGlobals.Config.PromptBreak)
@@ -4556,7 +4551,6 @@ namespace GenieClient
                                 sText = System.Environment.NewLine + sText;
                             }
                         }
-                        
                     }
                 }
             }
