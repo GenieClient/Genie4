93aef494000f08e2ceacfa7213a8c6125b6d0b87
Djordje <cyphertinker@gmail.com>
2022-04-07 20:19:41 -0400

    added PromptForce setting

--- Core/Game.cs
+++ Core/Game.cs
@@ -785,6 +785,11 @@ namespace GenieClient.Genie
                 {
                     m_bBold = false;
                 }
+
+                if (m_oGlobals.Config.PromptForce && m_oTargetWindow == WindowTarget.Main)
+                {
+                    PrintTextWithParse(m_oGlobals.Config.sPrompt, true, 0);
+                }
             }
         }
 
