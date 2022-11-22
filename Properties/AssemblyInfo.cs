98f631b5ba372bcd4c0ba89dbcb333db26d3ad02
Djordje <cyphertinker@gmail.com>
2022-11-22 07:43:18 -0500

    corrected version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-[assembly: AssemblyVersion("4.0.2.501")]
-[assembly: AssemblyFileVersion("4.0.2.501")] 
+[assembly: AssemblyVersion("4.0.2.5")]
+[assembly: AssemblyFileVersion("4.0.2.5")] 
