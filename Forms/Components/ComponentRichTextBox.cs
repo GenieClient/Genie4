387b43b2f9e760917e882729db4d2afb97b035ad
Thires <thiresdr@gmail.com>
2022-12-03 00:33:04 -0500

    Add files via upload

--- Forms/Components/ComponentRichTextBox.cs
+++ Forms/Components/ComponentRichTextBox.cs
@@ -52,6 +52,15 @@ namespace GenieClient
         private const int SCF_WORD = 0x2;
         private const int SCF_ALL = 0x4;
 
+        private const uint WM_VSCROLL = 0x115;
+        private const uint SB_LINEUP = 0;
+        private const uint SB_LINEDOWN = 1;
+        private const uint SB_PAGEUP = 2;
+        private const uint SB_PAGEDOWN = 3;
+        private const uint SB_TOP = 6;
+        private const uint SB_BOTTOM = 7;
+        private const uint SB_ENDSCROLL = 8;
+
         /* TODO ERROR: Skipped RegionDirectiveTrivia */
         private const uint CFE_BOLD = 1;
         private const uint CFE_ITALIC = 2;
@@ -196,11 +205,11 @@ namespace GenieClient
                 {
                     if (sText.StartsWith(" "))
                     {
-                        return "[" + Strings.FormatDateTime(DateAndTime.Now, DateFormat.ShortTime) + "]";
+                        return "[" + Strings.FormatDateTime(DateAndTime.Now, DateFormat.LongTime) + "]";
                     }
                     else
                     {
-                        return "[" + Strings.FormatDateTime(DateAndTime.Now, DateFormat.ShortTime) + "] ";
+                        return "[" + Strings.FormatDateTime(DateAndTime.Now, DateFormat.LongTime) + "] ";
                     }
                 }
             }
@@ -538,11 +547,8 @@ namespace GenieClient
             {
                 ParseVolatileHighlights(m_oParentForm.Globals.RoomObjects);
             }
-
             // Presets and Bold
             ParseVolatileHighlights(m_oParentForm.Globals.VolatileHighlights);
-
-
             // Highlight String
             if (!Information.IsNothing(m_oParentForm.Globals.HighlightList.RegexString))
             {
@@ -852,6 +858,40 @@ namespace GenieClient
             EventKeyPress?.Invoke(e);
             e.Handled = true;
         }
+
+        // No Scrollbars
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hWnd, uint msg, int wParam, IntPtr lParam);
+
+        public const int SB_LINELEFT = 0;
+        public const int SB_LINERIGHT = 1;
+        public const int SB_PAGELEFT = 2;
+        public const int SB_PAGERIGHT = 3;
+        public const int SB_THUMBPOSITION = 4;
+        public const int SB_THUMBTRACK = 5;
+        public const int SB_LEFT = 6;
+        public const int SB_RIGHT = 7;
+
+        public static uint MakeWord(byte low, byte high)
+        {
+            return ((uint)high << 8) | low;
+        }
+
+        public void ComponentRichTextBox_MouseWheel(object sender, MouseEventArgs e)
+        {
+            int bLines = e.Delta * SystemInformation.MouseWheelScrollLines / 120;
+            if (bLines > 0)
+            {
+                for (int i = 0; i < bLines; i++)
+                    SendMessage(Handle, WM_VSCROLL, (int)MakeWord((byte)SB_LINEUP, 0), IntPtr.Zero);
+            }
+            else
+            {
+                for (int i = bLines; i < 0; i++)
+                    SendMessage(Handle, WM_VSCROLL, (int)MakeWord((byte)SB_LINEDOWN, 0), IntPtr.Zero);
+            }
+        }
+        /// End No Scrollbars
 
         private bool m_bMouseDown = false;
 
